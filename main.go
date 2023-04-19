a11fd061b35ecd3cea8554ce43d19cbef4bb79ae
Geoffrey <tipton@mysecretroom.com>
2023-04-19 21:37:40 +0800

    tiidy

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
-import (
-	"all-news/lib"
-	"all-news/route"
-	"all-news/rss"
-	"all-news/sql"
+import ( // after go mod init [app name] add the app name to these modules
+	"[app name]/conf"
+	"[app name]/lib"
+	"[app name]/route"
+	"[app name]/sql"
 )
 
 /*
-https://medium.com/@vosmith08/scraping-the-web-in-go-b85ac37c14df
-Done: Read folder for RSS files, Change them to json format. Add Category, that identifies the Table.
-Done: Add paramters in the RSS list for Category, Target Media Telegram etc, and keys.
-{	category:"",
-	media:[{"Title":"Telegram", "KEY":"2o387230812308"},{"TITLE":"FaceBook","KEY":"09809098098098"}],
-	rss:[	"http:\\ksajhdsajhlsj",
-			"http:\\otherurl"    ]
-}
+	Welcome to the Template for starting
 */
 
 /***********************_**************
@@ -26,7 +19,7 @@ Done: Add paramters in the RSS list for Category, Target Media Telegram etc, and
  *     |_| |_| |_|\__,_|_|_| |_|
  * ---------------------------------- */
 func main() {
-	defer func() {
+	defer func() { //This is a normal trap to prevent app crashing out. no point in main, but higher functions works well.
 		r := recover()
 		if r != nil {
 			lib.Error("Application Failure:", r)
@@ -34,17 +27,7 @@ func main() {
 	}()
 	lib.Info("Initialize Database")
 	sql.InitDB()
-	lib.Info("Initilize Stay Alive")
-	go rss.StayAlive()
-	lib.Info("Initilize Feed inport")
-	rss.Init() //Initialise and load json files for the feed
-	go rss.LoadFeeds() // Get the RSS feeds
 	lib.Info("Initilize Posting to Channels")
-	go rss.Post() // Post to various channels etc.
-	lib.Info("Initilize Post Clean up")
-	go rss.Cleanposts() // Remove old posts.
-	//go openDiscord()
 
 	route.Init()
-
 }
